11a1eca7bdb7fd845968b5df9d27534a0661e193
Hrvoje Stojic <hrvoje.stojic@protonmail.com>
2021-04-21 21:37:59 +0100

    Update models.py
    added imports and CRWInitialStateModel class

--- src/snc/agents/rl/models.py
+++ src/snc/agents/rl/models.py
@@ -1,11 +1,23 @@
-@gin.configurable
+import numpy as np
+import tensorflow as tf
+
+from tf_agents.environments.tf_py_environment import TFPyEnvironment
+
+from bellman.environments.initial_state_distribution_model import (
+    DeterministicInitialStateModel,
+)
+from bellman.environments.reward_model import RewardModel
+from bellman.environments.termination_model import TerminationModel
+
+
 class CRWRewardModel(RewardModel):
     """
     Reward function for the controlled random walk environment, based on cost_per_buffer.
+    Information from the environment is neeeded.
     """
     def __init__(self, env: TFPyEnvironment):
         self.cost_per_buffer = env.cost_per_buffer
-        super.__init__(self)
+        super().__init__()
 
     def _step_reward(
         self, observation: tf.Tensor, action: tf.Tensor, next_observation: tf.Tensor
@@ -13,3 +25,14 @@ class CRWRewardModel(RewardModel):
         cost = np.dot(self.cost_per_buffer.transpose(), observation)
         reward = - float(cost)
         return tf.cast(reward, self._reward_spec.dtype)
+
+
+class CRWInitialStateModel(DeterministicInitialStateModel):
+    """
+    Initial state model for the the controlled random walk environment.
+    Information from the environment is neeeded.
+    """
+
+    def __init__(self, env: TFPyEnvironment):
+        self.initial_state = env.state_initialiser.get_initial_state()
+        super().__init__(state=self.initial_state)
